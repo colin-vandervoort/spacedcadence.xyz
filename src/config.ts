29651b548474531f378c5a0f205fc73e1d709d1f
Colin VanDervoort <colin.vandervoort.42@gmail.com>
2023-03-11 22:38:25 -0700

    minor fixes/refactoring, add bio section in footer

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'spacedcadence';
+export const SITE_TITLE = 'spacedcadence.xyz';
 export const SITE_DESCRIPTION = 'Colin talks about computers';
 
 export const U_DIAMOND = "⬩";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,14 +8,21 @@ module.exports = {
 	theme: {
 		extend: {
 			gridTemplateRows: {
+				'body': 'auto 1fr auto',
 				'header': '1fr auto',
+				'auto-rest': 'auto 1fr',
 			},
 			gridTemplateColumns: {
 				'header': '1fr auto',
+				'site-md': 'minmax(0, 1fr) minmax(0, 3fr)',
+				'site-lg': 'minmax(0, 1fr) minmax(0, 4fr) minmax(0, 1fr)',
 			},
-			// listStyleType: {
-			// 	'diamond': '⬩',
-			// }
+			listStyleType: {
+				'diamond': '"⬩"',
+			},
+			width: {
+				'32': '8rem',
+			},
 		},
 		fontFamily: {
 			'sans': [
